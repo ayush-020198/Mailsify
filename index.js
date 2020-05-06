7bc21bede9d6b8babb05d81cd1f8bbb55a39b2ac
Ayush Shrivastava <ayush.shrivastavaujn@gmail.com>
2020-05-06 10:06:27 +0530

    initial commit

---
+++ index.js
@@ -0,0 +1,9 @@
+const express = require("express");
+const app = express();
+
+app.get("/", (req, res) => {
+	res.send({ Hello: "Friends" });
+});
+
+const PORT = process.env.PORT || 5000;
+app.listen(console.log("Running on port " + PORT));

d28a1b26f6db3d3b4ab421442f612bc288eb2f2b
Ayush Shrivastava <ayush.shrivastavaujn@gmail.com>
2020-05-16 15:25:20 +0530

    Adding redux to the project

--- client/src/index.js
+++ client/src/index.js
@@ -1,4 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleware } from 'redux';
+import App from './components/App';
 
-ReactDOM.render();
+const store = createStore(() => [], {}, applyMiddleware());
+
+ReactDOM.render(
+	<Provider store={store}>
+		<App />
+	</Provider>,
+	document.querySelector('#root')
+);

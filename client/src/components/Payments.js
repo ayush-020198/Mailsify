6386854f2ef7513ef970cb3e0338d899c3507ffa
Ayush Shrivastava <ayush.shrivastavaujn@gmail.com>
2020-05-18 21:30:48 +0530

    adding stripe first stage

---
+++ client/src/components/Payments.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import StripeCheckout from 'react-stripe-checkout';
+
+class Payments extends Component {
+	render() {
+		return (
+			<StripeCheckout
+				amount={500}
+				token={(token) => console.log(token)}
+				stripeKey={process.env.REACT_APP_STRIPE_KEY}
+			/>
+		);
+	}
+}
--- config/prod.js
+++ config/prod.js
@@ -3,4 +3,6 @@ module.exports = {
 	googleClientSecret: process.env.GOOGLE_CLIENT_SECRET,
 	mongooseURI: process.env.MONGOOSE_URI,
 	cookieKey: process.env.COOKIE_KEY,
+	stripePublishableKey: process.env.STRIPE_PUBLISHABLE_KEY,
+	stripeSecretKey: process.env.STRIPE_SECRET_KEY,
 };

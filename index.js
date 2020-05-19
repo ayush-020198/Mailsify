6dd000d7429a3a8bdf9b7d9162d00161c721eff3
Ayush Shrivastava <ayush.shrivastavaujn@gmail.com>
2020-05-19 17:46:51 +0530

    heroku deployment stage-2

--- index.js
+++ index.js
@@ -25,6 +25,14 @@ app.use(passport.session());
 require('./routes/authRoutes')(app);
 require('./routes/billingRoutes')(app);
 
+if (process.env.NODE_ENV === 'production') {
+	app.use(express.static('client/build'));
+
+	app.get('*', (req, res) => {
+		res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+	});
+}
+
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT);

f9f44db9582d27e9828675ebd37b9831d17705b5
Ayush Shrivastava <ayush.shrivastavaujn@gmail.com>
2020-05-25 13:56:28 +0530

    Getting from dashboard to surveynew

--- client/src/components/Dashboard.js
+++ client/src/components/Dashboard.js
@@ -1,8 +1,18 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+
 const Dashboard = () => {
 	return (
 		<div>
-			<h1>Dashboard</h1>
+			<h1>This is the Dashboard</h1>
+			<div className="fixed-action-btn">
+				<Link
+					to="/surveys/new"
+					className="btn-floating btn-large waves-effect waves-light red"
+				>
+					<i className="large material-icons">add</i>
+				</Link>
+			</div>
 		</div>
 	);
 };

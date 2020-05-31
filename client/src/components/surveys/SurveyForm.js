cff220ce994ce8c61f973560670b63ce6dabf791
Ayush Shrivastava <ayush.shrivastavaujn@gmail.com>
2020-05-31 17:28:21 +0530

    Persisting Form Fields

--- client/src/components/surveys/SurveyForm.js
+++ client/src/components/surveys/SurveyForm.js
@@ -90,4 +90,5 @@ function validate(values) {
 export default reduxForm({
 	validate,
 	form: 'surveyForm',
+	destroyOnUnmount: false,
 })(SurveyForm);
--- client/src/components/surveys/SurveyFormReview.js
+++ client/src/components/surveys/SurveyFormReview.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { connect } from 'react-redux';
 
 const SurveyFormReview = ({ onCancel }) => {
 	return (
@@ -11,4 +12,8 @@ const SurveyFormReview = ({ onCancel }) => {
 	);
 };
 
-export default SurveyFormReview;
+function mapStateToProps(state) {
+	return { formValues: state.form.surveyForm.values };
+}
+
+export default connect(mapStateToProps)(SurveyFormReview);
